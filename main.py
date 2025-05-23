c97a1f8efd4c432e1aafddff8dc25c334d624327
Marco Coraggio <marcocoraggio@live.com>
2025-05-23 17:20:36 +0200

    some commented line to test some data

--- main.py
+++ main.py
@@ -44,6 +44,18 @@ col_names_ref_frame_baseline_points = [['p1_x','p1_y','p1_z'],['p2_x','p2_y','p2
 # time_step = 0.01
 # is_use_baseline = False;  time_step_baseline = None
 
+# file_path_estimand = r"data\dfki_mocap_excerises_2025-03-25\exercise_1\positions.csv"
+# col_names_pos_estimand  = ["X22", "Y22", "Z22", "X26", "Y26", "Z26"] # Rshoulder and Lshoulder
+# discarded_time = 5; look_behind_pcent = 10; look_ahead_pcent = 40; listening_time = 20; time_const_lowpass_filter_estimand_pos = 0.1
+# rows_to_skip_estimand = list(range(0, 4))
+# time_step = 1/30
+# is_use_baseline = False;  time_step_baseline = None
+
+# file_path_estimand = r"data\cyens_mocap_2025-04-23\csv\Ex2-Sub1-0_worldpos.csv"; col_names_pos_estimand = ["Hips.X","Hips.Y","Hips.Z","RightShoulder.X","RightShoulder.Y","RightShoulder.Z","LeftShoulder.X","LeftShoulder.Y","LeftShoulder.Z","RightHand.X","RightHand.Y","RightHand.Z","LeftHand.X","LeftHand.Y","LeftHand.Z"];  look_behind_pcent = 5; look_ahead_pcent = 15; listening_time = 10; time_const_lowpass_filter_estimand_pos = 0.1
+# rows_to_skip_estimand = list(range(0, 0))
+# is_use_baseline = False
+# time_step = 0.016667
+
 
 # Load data
 # ------------------------------------------------
